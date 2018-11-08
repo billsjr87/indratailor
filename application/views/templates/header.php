b64691f17833480c174249189d20f66c3a55a260
unknown <bills.jr87@gmail.com>
2018-11-08 22:12:59 +0700

    Commit #1 - add codeigniter set default config add bootstrap 4 add jquery add popper

---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Indra Tailor | IIS</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+  </head>
+  <body>
