39ceac286d67e27567ba42ffd894ece08d5ae2b3
unknown <bills.jr87@gmail.com>
2018-11-19 07:33:36 +0700

    Commit #1 add more method in Order Controller

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -53,23 +53,30 @@ class Order extends CI_Controller {
 		$ordr_dopy = $this->input->post("ordr_dopy");
 		$ordr_accr = $this->input->post("ordr_accr");
 
+		$pymt_stat = 0;
+		if ($ordr_dopy == $ordr_fees and $ordr_accr == 0) {
+			$pymt_stat = 1;
+		}
+
 		$ordr_data = array(
 			'ordr_nmbr' => $ordr_nmbr,
 			'ordr_date' => $ordr_date,
 			'ordr_fndt' => $ordr_fndt,
 			'cust_indx' => $cust_indx,
 			'ordr_detl' => $ordr_detl,
+			'ordr_fees' => $ordr_fees
 		);
 
+		// TODO: read order data...
 		// TODO: add order to database table order
 		// TODO: add items to database table order_item
 		// TODO: add account to database table account
 
 		$acco_data = array(
 			'ordr_nmbr' => $ordr_nmbr,
-			'ordr_fees' => $ordr_fees,
-			'ordr_dopy' => $ordr_dopy,
-			'ordr_accr' => $ordr_accr
+			'accr_pydt' => $ordr_date,
+			'accr_pymt' => $ordr_dopy,
+			'accr_stat' => $pymt_stat
 		);
 	}
 
